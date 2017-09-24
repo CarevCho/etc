fb0f6fc004084fd27a4654f684bf01f97969c7c9
Carev <hyeonee00@gmail.com>
2017-09-24 20:39:46 +0900

    # file input & output example ( cpp & c )

---
+++ file_input_output/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+#if 1
+// file write
+int main(int argc, char* argv[])
+{
+#if 0
+	/// cpp
+	ofstream outfile("output.txt");
+
+	for (int i = 0; i < 10; i++) {
+		outfile << i << endl;
+	}
+	outfile.close();
+#else
+	/// c
+	FILE *file;
+	file = fopen("output.txt", "w");
+	for (int i = 0; i < 10; i++) {
+		fprintf(file, "%d", i);
+	}fclose(file);
+
+#endif
+    return 0;
+}
+
+
+#else
+// file read
+int main(int argc, char*argv[]) 
+{
+#if 1
+	/// cpp
+#else
+	/// c
+#endif
+	return 0;
+}
+
+
+#endif
