d6367f6be0eefbe16bf8a84d6a367e2be211dd37
Carev <hyeonee00@gmail.com>
2017-09-24 20:25:58 +0900

    # mouse callback about exist image to find hole point

--- opencv_mouse_callback/main.cpp
+++ opencv_mouse_callback/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "src\opencv_3_3_0.h"
 
+///
+#include <io.h>
+#include <string>
+///
+
 using namespace std;
 using namespace cv;
 
@@ -23,29 +28,45 @@ int main()
 
 	namedWindow("", CV_NORMAL);
 
-	while (1) {
-		vid >> frame;		// read frame from device
-		if (frame.empty())
-			break;
-		
-		///
-		// initialize parameter about mouse component
-		data.center = Point2i(0, 0);
-		data.image = frame;
-		///
+	string path = "D:\\MMMIL\\Project\\CapsuleEndoscopy\\data\\simulation\\*.*";
+	cv::String dirPath = "D:\\MMMIL\\Project\\CapsuleEndoscopy\\data\\simulation\\";
+	struct _finddata_t fd;
+	intptr_t handle;
+	int number = 0;
+	if ((handle = _findfirst(path.c_str(), &fd)) == -1L)
+		cout << "No file in directory!" << endl;
+	do
+	{
+		String file = fd.name;
+		//cout << fd.name << endl;
+		cout << dirPath + file << endl;
+		if (number >= 2) {
 
-		// hold function to window to control
-		setMouseCallback("", onMouse, &data);
-		imshow("", frame);
-		
+			frame = imread(dirPath + file, IMREAD_GRAYSCALE);		// read frame from device
+			if (frame.empty())
+				break;
+			Mat grayimg = frame(Rect(5, 0, frame.cols - 20, frame.rows));
+			equalizeHist(grayimg, grayimg);
 
-		int key = waitKey();
-		if (key == 27)
-			break;
-		else if (key == 97)
-			continue;
+			///
+			// initialize parameter about mouse component
+			data.center = Point2i(0, 0);
+			data.image = grayimg;
+			///
 
-	}
+			// hold function to window to control
+			setMouseCallback("", onMouse, &data);
+			imshow("", grayimg);
+
+
+			int key = waitKey();
+			if (key == 27)
+				break;
+			else if (key == 97)
+				continue;
+		}number++;
+	} while (_findnext(handle, &fd) == 0);
+	_findclose(handle);
 
     return 0;
 }
@@ -56,7 +77,7 @@ void onMouse(int event, int x, int y, int flags, void* userdata)
 	inputdata *p = (inputdata*)userdata;
 	if (event == CV_EVENT_LBUTTONDOWN) {
 		p->center.x = x; p->center.y = y;
-		circle(p->image, p->center, 5, Scalar(0), -1);	
+		circle(p->image, p->center, 5, Scalar(255), -1);	
 		imshow("", p->image);
 	}
 }
