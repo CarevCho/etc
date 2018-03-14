5a9c9b58e09f0002097a5bbdac7fca875b5b4a8e
carev <hyeonee00@gmail.com>
2018-03-14 12:05:07 +0900

    # mfc; opencv; webcam - mfc webcam example

---
+++ ex_mfc_diag_opencv_webcam/opencv_3_4_1.h
@@ -0,0 +1,12 @@
+#ifndef _OPENCV_3_4_1_
+
+#define _OPENCV_3_4_1_
+#include "opencv2\opencv.hpp"
+
+#if _DEBUG
+#pragma comment(lib, "opencv_world341d.lib")
+#elif NDEBUG
+#pragma comment(lib, "opencv_world341.lib")
+#endif
+
+#endif
